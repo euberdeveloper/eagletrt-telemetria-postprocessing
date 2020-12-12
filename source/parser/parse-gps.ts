a71299aa3ee580fa097bec8866a9d0ede6907080
nicolatoscan <nicolatoscan99@outlook.it>
2020-12-12 05:11:55 +0100

    fix gps values

--- source/parser/parse-gps.ts
+++ source/parser/parse-gps.ts
@@ -1,56 +1,67 @@
-function parseCoordinates(raw: number) {
-    raw /= 100
-    const left = Math.floor(raw)
-    const right = (raw - left) * (5/3)
-    return (left + right) * 100
+import { GPSData } from '../types';
+
+function parseCoordinates(raw: string) {
+    let value = parseFloat(raw);
+    const temp = +value / 100;
+    const left = Math.floor(temp);
+    const right = (temp - left) * (5 / 3);
+    return left + right;
 }
 
 function parseGpsValues (
     msg: string[],
-    callback: (props: string[], value: any) => void
+    callback: (props: string[], value: GPSData) => void
 ) {
     if (msg[0] === '$GNGGA') {
         callback(['gps', 'new', 'gga'], {
-            'utc_time': msg[1],
-            'latitude': parseCoordinates(parseFloat(msg[2])),
-            'ns_indicator': msg[3],
-            'longitude': parseCoordinates(parseFloat(msg[4])),
-            'ew_indicator': msg[5],
-            'status': parseInt(msg[6]),
-            'altitude': parseFloat(msg[11]),
+            utc_time: parseFloat(msg[1]),
+            latitude: parseCoordinates(msg[2]),
+            longitude: parseCoordinates(msg[4]),
+            altitude: parseFloat(msg[11]),
+            speed_knots: NaN,
+            course: NaN,
+            // ew_indicator: msg[5],
+            // status: parseInt(msg[6]),
+            // ns_indicator: msg[3],
         })
     }
 
     if (msg[0] === '$GNGLL') {
         callback(['gps', 'new', 'gll'], {
-            'latitude': parseCoordinates(parseFloat(msg[1])),
-            'ns_indicator': msg[2],
-            'longitude': parseCoordinates(parseFloat(msg[3])),
-            'ew_indicator': msg[4],
-            'utc_time': msg[5],
-            'status': msg[6] === 'A',
+            utc_time: parseFloat(msg[5]),
+            latitude: parseCoordinates(msg[1]),
+            longitude: parseCoordinates(msg[3]),
+            altitude: NaN,
+            speed_knots: NaN,
+            course: NaN,
+            // ns_indicator: msg[2],
+            // ew_indicator: msg[4],
+            // status: msg[6] === 'A',
         })
     }
 
     if (msg[0] === '$GNRMC') {
         callback(['gps', 'new', 'rmc'], {
-            'utc_time': msg[1],
-            'status': msg[2] === 'A',
-            'latitude': parseCoordinates(parseFloat(msg[3])),
-            'ns_indicator': msg[4],
-            'longitude': parseCoordinates(parseFloat(msg[5])),
-            'ew_indicator': msg[6],
-            'ground_speed_knots': parseFloat(msg[7]),
-            'date': msg[9],
+            utc_time: parseFloat(msg[1]),
+            latitude: parseCoordinates(msg[3]),
+            longitude: parseCoordinates(msg[5]),
+            altitude: NaN,
+            speed_knots: parseFloat(msg[7]),
+            course: parseFloat(msg[8]),
+            // status: msg[2] === 'A',
+            // ns_indicator: msg[4],
+            // ew_indicator: msg[6],
+            // ground_speed_knots: parseFloat(msg[7]),
+            // date: msg[9],
         })
     }
 
-    if (msg[0] === '$GNVTG') {
-        callback(['gps', 'new', 'vtc'], {
-            'ground_speed_knots': parseFloat(msg[5]),
-            'ground_speed_human': parseFloat(msg[7]),
-        })
-    }
+    // if (msg[0] === '$GNVTG') {
+    //     callback(['gps', 'new', 'vtc'], {
+    //         ground_speed_knots: parseFloat(msg[5]),
+    //         ground_speed_human: parseFloat(msg[7]),
+    //     })
+    // }
 }
 
 export default function (
--- source/types/index.ts
+++ source/types/index.ts
@@ -1,3 +1,12 @@
 export enum EType {
     CSV, JSON, Test
 }
+
+export interface GPSData {
+    utc_time: number,
+    latitude: number,
+    longitude: number,
+    altitude: number,
+    speed_knots: number,
+    course: number
+}
