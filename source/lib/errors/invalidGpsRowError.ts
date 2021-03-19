badb8aef04aaca857dd5f5aab09c1c2dec0cb6d0
euberdeveloper <euberdeveloper@gmail.com>
2021-03-19 15:03:20 +0100

    feat(errors): add index info to errors
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/lib/errors/invalidGpsRowError.ts
+++ source/lib/errors/invalidGpsRowError.ts
@@ -3,10 +3,12 @@ import { EagletrtPostProcessingError } from './postProcessingError';
 export class EagletrtPostProcessingInvalidGpsRowError extends EagletrtPostProcessingError {
     protected static readonly DEFAULT_MESSAGE: string = 'Invalid gps log row';
     protected readonly line: string | null;
+    protected readonly index: number | null;
 
-    public constructor(message?: string, line?: string) {
+    public constructor(message?: string, line?: string, index?: number) {
         super(message ?? EagletrtPostProcessingInvalidGpsRowError.DEFAULT_MESSAGE);
         this.line = line ?? null;
+        this.index = index ?? null;
         this.name = 'EagletrtPostProcessingInvalidGpsRowError';
     }
 }
--- source/lib/errors/invalidGpsRowTimestamp.ts
+++ source/lib/errors/invalidGpsRowTimestamp.ts
@@ -3,8 +3,8 @@ import { EagletrtPostProcessingInvalidGpsRowError } from './invalidGpsRowError';
 export class EagletrtPostProcessingInvalidGpsRowTimestampError extends EagletrtPostProcessingInvalidGpsRowError {
     protected static readonly DEFAULT_MESSAGE: string = 'Invalid gps log row timestamp';
 
-    public constructor(message?: string, line?: string) {
-        super(message ?? EagletrtPostProcessingInvalidGpsRowTimestampError.DEFAULT_MESSAGE, line);
+    public constructor(message?: string, line?: string, index?: number) {
+        super(message ?? EagletrtPostProcessingInvalidGpsRowTimestampError.DEFAULT_MESSAGE, line, index);
         this.name = 'EagletrtPostProcessingInvalidGpsRowError';
     }
 }
