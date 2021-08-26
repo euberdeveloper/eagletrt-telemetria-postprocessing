a674cb46cf86df4b5f8314f4c326f86212727969
euberdeveloper <euberdeveloper@gmail.com>
2021-08-26 22:00:46 +0200

    fix: fix
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/lib/utils/cancicd/nets/secondary.ts
+++ source/lib/utils/cancicd/nets/secondary.ts
@@ -3,60 +3,69 @@ import * as byteify from 'byteify';
 
 export function serializeSecondaryBmsHvVoltage(data: ReturnType<typeof deserializeSecondaryBmsHvVoltage>) {
     return Uint8Array.from([
-        ...byteify.serializeUint16(data.max),
-        ...byteify.serializeUint16(data.min),
-        ...byteify.serializeUint32(data.total)
+        ...byteify.serializeUint16(data.max).reverse(),
+        ...byteify.serializeUint16(data.min).reverse(),
+        ...byteify.serializeUint32(data.total).reverse()
     ]);
 }
 export function deserializeSecondaryBmsHvVoltage(bytes) {
     return {
-        max: byteify.deserializeUint16(bytes.slice(0, 2)),
-        min: byteify.deserializeUint16(bytes.slice(2, 4)),
-        total: byteify.deserializeUint32(bytes.slice(4, 8))
+        max: byteify.deserializeUint16(bytes.slice(0, 2).reverse()),
+        min: byteify.deserializeUint16(bytes.slice(2, 4).reverse()),
+        total: byteify.deserializeUint32(bytes.slice(4, 8).reverse())
     };
 }
 
 export function serializeSecondaryBmsHvTemperature(data: ReturnType<typeof deserializeSecondaryBmsHvTemperature>) {
     return Uint8Array.from([
-        ...byteify.serializeUint16(data.average),
-        ...byteify.serializeUint16(data.max),
-        ...byteify.serializeUint16(data.min)
+        ...byteify.serializeUint16(data.average).reverse(),
+        ...byteify.serializeUint16(data.max).reverse(),
+        ...byteify.serializeUint16(data.min).reverse()
     ]);
 }
 export function deserializeSecondaryBmsHvTemperature(bytes) {
     return {
-        average: byteify.deserializeUint16(bytes.slice(0, 2)),
-        max: byteify.deserializeUint16(bytes.slice(2, 4)),
-        min: byteify.deserializeUint16(bytes.slice(4, 6))
+        average: byteify.deserializeUint16(bytes.slice(0, 2).reverse()),
+        max: byteify.deserializeUint16(bytes.slice(2, 4).reverse()),
+        min: byteify.deserializeUint16(bytes.slice(4, 6).reverse())
     };
 }
 
 export function serializeSecondaryBmsHvCurrent(data: ReturnType<typeof deserializeSecondaryBmsHvCurrent>) {
-    return Uint8Array.from([...byteify.serializeUint16(data.current), ...byteify.serializeUint16(data.pow)]);
+    return Uint8Array.from([
+        ...byteify.serializeUint16(data.current).reverse(),
+        ...byteify.serializeUint16(data.pow).reverse()
+    ]);
 }
 export function deserializeSecondaryBmsHvCurrent(bytes) {
     return {
-        current: byteify.deserializeUint16(bytes.slice(0, 2)),
-        pow: byteify.deserializeUint16(bytes.slice(2, 4))
+        current: byteify.deserializeUint16(bytes.slice(0, 2).reverse()),
+        pow: byteify.deserializeUint16(bytes.slice(2, 4).reverse())
     };
 }
 
 export function serializeSecondaryBmsHvErrors(data: ReturnType<typeof deserializeSecondaryBmsHvErrors>) {
-    return Uint8Array.from([...byteify.serializeUint8(data.fault_id), ...byteify.serializeUint8(data.fault_index)]);
+    return Uint8Array.from([
+        ...byteify.serializeUint8(data.fault_id).reverse(),
+        ...byteify.serializeUint8(data.fault_index).reverse()
+    ]);
 }
 export function deserializeSecondaryBmsHvErrors(bytes) {
     return {
-        fault_id: byteify.deserializeUint8(bytes.slice(0, 1)),
-        fault_index: byteify.deserializeUint8(bytes.slice(1, 2))
+        fault_id: byteify.deserializeUint8(bytes.slice(0, 1).reverse()),
+        fault_index: byteify.deserializeUint8(bytes.slice(1, 2).reverse())
     };
 }
 
 export function serializeSecondaryBmsHvWarnings(data: ReturnType<typeof deserializeSecondaryBmsHvWarnings>) {
-    return Uint8Array.from([...byteify.serializeUint8(data.fault_id), ...byteify.serializeUint8(data.fault_index)]);
+    return Uint8Array.from([
+        ...byteify.serializeUint8(data.fault_id).reverse(),
+        ...byteify.serializeUint8(data.fault_index).reverse()
+    ]);
 }
 export function deserializeSecondaryBmsHvWarnings(bytes) {
     return {
-        fault_id: byteify.deserializeUint8(bytes.slice(0, 1)),
-        fault_index: byteify.deserializeUint8(bytes.slice(1, 2))
+        fault_id: byteify.deserializeUint8(bytes.slice(0, 1).reverse()),
+        fault_index: byteify.deserializeUint8(bytes.slice(1, 2).reverse())
     };
 }
